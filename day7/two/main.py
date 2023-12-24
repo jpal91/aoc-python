bdebf276dadfa85a598b0f9e503f23e10ee26434
jpal91 <pallanschja@gmail.com>
2023-12-24 16:56:55 -0500

    finished day7 pt1

---
+++ day7/two/main.py
@@ -0,0 +1,59 @@
+from collections import Counter
+
+puzzle_test = """\
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483\
+"""
+
+CARDS = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'][::-1]
+
+def get_hand(h: list[str]) -> tuple[str, str, int]:
+    hand, bid = h
+    score = 0
+
+    hand_count = Counter(hand)
+    mc = hand_count.most_common()
+
+    if mc[0][1] == 5:
+        score += 60
+    elif mc[0][1] == 4:
+        score += 50
+    elif mc[0][1] == 3:
+        score += 30
+        if mc[1][1] == 2:
+            score += 10
+    elif mc[0][1] == 2:
+        score += 10
+        if mc[1][1] == 2:
+            score += 10
+    
+    return hand, bid, score
+
+def sort_hand(hands: list[tuple[str, str, int]]) -> list:
+    return sorted(
+        hands, 
+        key = lambda x: (x[2], *(CARDS.index(c) for c in x[0])), 
+    )
+
+def parse(p: str) -> list:
+    p = p.split('\n')
+    return [line.split() for line in p]
+
+if __name__ == '__main__':
+    with open('day7/one/puzzle.txt') as f:
+        puzzle = parse(f.read())
+
+    # puzzle = parse(puzzle_test)
+    
+    hands = [get_hand(p) for p in puzzle]
+    sorted_hand = sort_hand(hands)
+    
+    total = 0
+
+    for i, hand in enumerate(sorted_hand):
+        total += (int(hand[1]) * (i + 1))
+    
+    print(total)
