ae5f470432a8d3c1c1c81c1e64c0dd4427a724c2
jpal91 <pallanschja@gmail.com>
2023-12-24 14:18:29 -0500

    finished day6

--- day6/two/main.py
+++ day6/two/main.py
@@ -1,4 +1,3 @@
-import re
 
 puzzle_test = """\
 Time:      7  15   30
@@ -8,17 +7,43 @@ Distance:  9  40  200
 def farthest(held: int, max: int) -> int:
     return (max - held) * held
 
+def binary_search(pos_range: range, max_time: int, max_dist: int, rev: bool) -> int:
+    l, r = 0, pos_range[-1]
+
+    while l <= r:
+        mid = (r + l) // 2
+        wins = farthest(mid, max_time) > max_dist
+
+        if wins:
+            if not rev:
+                r = mid - 1
+            else:
+                l = mid + 1
+        else:
+            if not rev:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        if l == r:
+            if not rev:
+                return mid + 1
+            else:
+                return mid - 1
+
 if __name__ == '__main__':
     with open('day6/one/puzzle.txt') as f:
         puzzle = f.read()
 
     t, d = puzzle.strip().split('\n')
-    times = map(int, t[5:].split())
-    dists = map(int, d[9:].split())
+    time = int("".join(map(str, t[5:].split())))
+    dist = int("".join(map(str, d[9:].split())))
 
-    beats = 1
-    for time, dist in zip(times, dists):
-        best = len(list(filter(lambda x: x[1] > dist, [ (t, farthest(t, time)) for t in range(time + 1) ])))
-        beats *= best
-        print(best)
-    print(beats)
+    bottom, top = 0, time
+
+    half1 = range(0, time // 2)
+    half2 = range(time // 2, time + 1)
+    search1 = binary_search(half1, time, dist, False)
+    search2 = binary_search(half2, time, dist, True)
+    print(search1, search2)
+    print(search2 - search1)
