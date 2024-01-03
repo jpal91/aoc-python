4cb6be5074dcf599958f69441fa0772726902a2c
jpal91 <pallanschja@gmail.com>
2024-01-03 18:41:38 -0500

    finished 21.1

---
+++ day21/one/main.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from collections import deque
+
+sys.path.append(os.path.expanduser('~/dev/aoc'))
+from grid.grid import Grid, Cell
+
+puzzle_test = """\
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+"""
+
+def bfs(node: Cell):
+    queue = deque([(node, 0)])
+    plots = set()
+    visited = set()
+
+    while queue:
+        node, steps = queue.popleft()
+
+        if (node, steps) in visited or steps >= max_steps:
+            if steps == max_steps:
+                node.value = 'O'
+                plots.add(node)
+            continue
+        visited.add((node, steps))
+
+        for n in node.act_neighbors:
+            if n.value == '#' or n in visited:
+                continue
+            queue.append((n, steps + 1))
+    
+    return plots
+
+if __name__ == '__main__':
+    with open('day21/one/puzzle.txt') as f:
+        puzzle = f.read()
+    
+    # puzzle = puzzle_test
+
+    grid = Grid.from_str(puzzle)
+    max_steps = 64
+
+    for cell in grid:
+        if cell.value == 'S':
+            res = bfs(cell)
+            break
+
+    print(len(res))
+    # print(grid)
